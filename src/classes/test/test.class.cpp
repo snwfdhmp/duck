74414939b2ea8e7df31d51c47f0c100183efe2a3
snwfdhmp <snwfdhmp@protonmail.com>
2017-05-01 00:33:17 +0200

    Quick commit

---
+++ src/classes/test/test.class.cpp
@@ -0,0 +1,15 @@
+// Project duck [duck managed]
+// Class test (src/classes/test/test.class.cpp)
+#ifndef TEST_CLASS_CPP
+#define TEST_CLASS_CPP
+
+//test class methods implementation
+
+#include "test.class.hpp"
+
+//class constructor
+test::test() {
+    // object initialization
+}
+
+#endif
---
+++ src/classes/test/test.class.hpp
@@ -0,0 +1,16 @@
+// Project duck [duck managed]
+// Class test (src/classes/test/test.class.hpp)
+#ifndef TEST_CLASS_HPP
+#define TEST_CLASS_HPP
+    
+//test class definition
+
+class test
+{
+public:
+    test(); //class constructor
+    ~test();
+        
+};
+
+#endif
